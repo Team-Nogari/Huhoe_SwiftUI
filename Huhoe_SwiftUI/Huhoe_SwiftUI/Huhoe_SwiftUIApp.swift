7454f632650340b8088edf365e6f4c31b72bd098
HJEHA <hyhpwang4@gmail.com>
2022-07-18 15:16:28 +0900

    project: 프로젝트 생성 #1

---
+++ Huhoe_SwiftUI/Huhoe_SwiftUI/Huhoe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Huhoe_SwiftUIApp.swift
+//  Huhoe_SwiftUI
+//
+//  Created by 황제하 on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct Huhoe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
