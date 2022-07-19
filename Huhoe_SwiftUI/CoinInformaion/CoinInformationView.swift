c6c9cdfd277e7371f1a6ef383eafd9fdc3cef640
HJEHA <hyhpwang4@gmail.com>
2022-07-19 22:37:03 +0900

    feat: CoinInformationView 구현 #4

--- Huhoe_SwiftUI/CoinInformaion/CoinInformationView.swift
+++ Huhoe_SwiftUI/CoinInformaion/CoinInformationView.swift
@@ -15,9 +15,95 @@ struct CoinInformationView: View {
     let coinInformation: CoinInformationModel
     
     var body: some View {
-        VStack {
-            Text("ddd")
+        WithViewStore (self.store) { viewStore in
+            VStack(spacing: 2) {
+                
+                // 코인 이름, 심볼
+                
+                HStack {
+                    Text(coinInformation.coinName)
+                        .font(.title2)
+                        .fontWeight(.black)
+                    
+                    Text(coinInformation.coinSymbol)
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(.gray)
+                }
+                .padding(.top, 8)
+                
+                // 구분선
+                
+                Rectangle()
+                    .fill(Color(.gray))
+                    .frame(height: 1)
+                    .padding(.horizontal, 8)
+                
+                // 계산된 투자 금액
+                
+                Text("\(coinInformation.calculatedPriceString)원")
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.red)
+                    .padding(0)
+                
+                // 손익
+                
+                HStack {
+                    Text("\(coinInformation.profitAndLossString)원")
+                        .font(.subheadline)
+                        .fontWeight(.black)
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.center)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        
+                    
+                    Text("\(coinInformation.rateString)%")
+                        .font(.subheadline)
+                        .fontWeight(.black)
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.center)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                }
+                .padding(.horizontal, 8)
+                
+                // 코인 가격(당시, 현재)
+                
+                HStack(spacing: 16) {
+                    HStack {
+                        Text("당시")
+                            .font(.callout)
+                            .fontWeight(.black)
+                        
+                        Text("\(coinInformation.oldPriceString)원")
+                            .font(.subheadline)
+                            .fontWeight(.black)
+                            .kerning(-1)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    
+                    HStack {
+                        Text("현재")
+                            .font(.callout)
+                            .fontWeight(.black)
+                        
+                        Text("\(coinInformation.currentPriceString)원")
+                            .font(.subheadline)
+                            .fontWeight(.black)
+                            .kerning(-1)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                }
+                .padding([.leading, .trailing, .bottom], 8)
+                
+            }
+            .background( // 그림자
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.white)
+                    .shadow(color: .gray, radius: 2, x: 3, y: 3)
+            )
         }
+        .padding(.horizontal, 8)
     }
 }
 
