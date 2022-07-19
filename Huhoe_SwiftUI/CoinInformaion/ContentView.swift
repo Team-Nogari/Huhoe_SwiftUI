42a7be195bea27e11be2411e6a258f2c24cd88f2
HJEHA <hyhpwang4@gmail.com>
2022-07-19 18:22:12 +0900

    refactor: CoinInformationView 이름 변경 #4

--- Huhoe_SwiftUI/CoinInformaion/ContentView.swift
+++ Huhoe_SwiftUI/CoinInformaion/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CoinInformationView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CoinInformationView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CoinInformationView()
     }
 }
